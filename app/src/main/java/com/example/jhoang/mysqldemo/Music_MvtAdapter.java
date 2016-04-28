c118ff8c26519e047a5d0973f0070f49ff6aab9b
kc0344 <kaichuan.chan@gmail.com>
2016-04-28 08:48:54 -0500

    clean buttons

--- app/src/main/java/com/example/jhoang/mysqldemo/Music_MvtAdapter.java
+++ app/src/main/java/com/example/jhoang/mysqldemo/Music_MvtAdapter.java
@@ -3,12 +3,11 @@ package com.example.jhoang.mysqldemo;
 /**
  * Created by chong on 4/23/2016.
  */
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 
 public class Music_MvtAdapter {
 
@@ -21,7 +20,6 @@ public class Music_MvtAdapter {
     public static final String KEY_CONTENT3 = "INSTRUMENT";
     public static final String KEY_CONTENT4 = "SHOWID";
 
-    //create table MY_DATABASE (ID integer primary key, Content text not null);
     private static final String SCRIPT_CREATE_DATABASE =
             "create table " + MYDATABASE_TABLE + " ("
                     + KEY_ID + " integer primary key autoincrement, "
@@ -39,11 +37,7 @@ public class Music_MvtAdapter {
         context = c;
     }
 
-   /* public Music_BookAdapter openToRead() throws android.database.SQLException {
-        sqLiteHelper = new SQLiteHelper(context, MYDATABASE_NAME, null, MYDATABASE_VERSION);
-        sqLiteDatabase = sqLiteHelper.getReadableDatabase();
-        return this;
-    }*/
+
 
     public Music_MvtAdapter openToWrite() throws android.database.SQLException {
         sqLiteHelper = new SQLiteHelper(context, MYDATABASE_NAME, null, MYDATABASE_VERSION);
@@ -54,29 +48,6 @@ public class Music_MvtAdapter {
     public void close(){
         sqLiteHelper.close();
     }
-
-    /*public long insert(String content1, String content2){
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(KEY_CONTENT1, content1);
-        contentValues.put(KEY_CONTENT2, content2);
-        return sqLiteDatabase.insert(MYDATABASE_TABLE, null, contentValues);
-    }
-
-    public int deleteAll(){
-        return sqLiteDatabase.delete(MYDATABASE_TABLE, null, null);
-    }
-
-    public void delete_byID(int id){
-        sqLiteDatabase.delete(MYDATABASE_TABLE, KEY_ID+"="+id, null);
-    }
-
-    public void update_byID(int id, String v1, String v2){
-        ContentValues values = new ContentValues();
-        values.put(KEY_CONTENT1, v1);
-        values.put(KEY_CONTENT2, v2);
-        sqLiteDatabase.update(MYDATABASE_TABLE, values, KEY_ID+"="+id, null);
-    }*/
 
     public Cursor queueAll(){
         String[] columns = new String[]{KEY_ID, KEY_CONTENT1, KEY_CONTENT2, KEY_CONTENT3, KEY_CONTENT4};
